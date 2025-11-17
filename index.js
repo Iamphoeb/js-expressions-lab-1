c31540cf480f2dfcb4d15074b9e1fcc703af1901
APPLE <vonenzell@gmail.com>
2025-11-17 15:53:49 +0300

    JS Project

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+const day1TempF = 32
+const day2TempC = 25
+const day3TempF = 70
+const day4TempC = 18
+const day5TempF = 80
+const day6TempC = 15
+const day7TempF = 72
+const day8TempC = 28
+const day9TempF = 68
+const day10TempC = 20
+const day11TempF = 75
+const day12TempC = 23
+const day13TempF = 82
+const day14TempC = 30
+const day15TempF = 65
+const day16TempC = 22
+const day17TempF = 77
+const day18TempC = 26
+const day19TempF = 78
+const day20TempC = 24
+const day21TempF = 73
+const day22TempC = 21
+const day23TempF = 79
+const day24TempC = 27
+const day25TempF = 71
+const day26TempC = 19
+const day27TempF = 74
+const day28TempC = 17
+const day29TempF = 76
+const day30TempC = 29
+const fahrenheitToCelsius = (fahrenheit) => (tempInfahrenheit - 32) * 5 / 9
+const celsiusToFahrenheit = (celsius) => (tempInCelsius * 9 / 5) + 32
+const temperaturesInFahrenheit = [day1TempF, day3TempF, day5TempF, day7TempF, day9TempF, day11TempF, day13TempF, day15TempF, day17TempF, day19TempF, day21TempF, day23TempF, day25TempF, day27TempF, day29TempF]
+const temperaturesInCelsius = [day2TempC, day4TempC, day6TempC, day8TempC, day10TempC, day12TempC, day14TempC, day16TempC, day18TempC, day20TempC, day22TempC, day24TempC, day26TempC, day28TempC, day30TempC]
+
+let tot_temperature_in_fahrenheit = 0
+let tot_temperature_in_celsius = 0
+
+for (let i = 0; i < temperaturesInFahrenheit.length; i++) {
+    tot_temperature_in_fahrenheit += temperaturesInFahrenheit[i]
+}
+
+for (let i = 0; i < temperaturesInCelsius.length; i++) {
+    tot_temperature_in_celsius += temperaturesInCelsius[i]
+}
+
+let avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / temperaturesInFahrenheit.length
+let avg_temperature_in_celsius = tot_temperature_in_celsius / temperaturesInCelsius.length
+
+console.log("Total Temperature in Fahrenheit: ", tot_temperature_in_fahrenheit)
+console.log("Total Temperature in Celsius: ", tot_temperature_in_celsius)
+console.log("Average Temperature in Fahrenheit: ", avg_temperature_in_fahrenheit)
+console.log("Average Temperature in Celsius: ", avg_temperature_in_celsius)     
+
+
+
+
+
 //! Start by creating the variables for the data recorded
 //* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
 
